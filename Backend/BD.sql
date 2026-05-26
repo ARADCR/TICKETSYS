d4aea1958e25f9695d1450b585e21cd60668fed9
ARADCR <axelraulaguilard@gmail.com>
2026-05-26 14:05:36 -0600

    se construyo la tabla users

--- Backend/BD.sql
+++ Backend/BD.sql
@@ -1,1 +1,7 @@
 CREATE DATABASE TICKETS
+create table users(
+    id int primary key identity(1,1),
+    name varchar(255) not null,
+    email varchar(255) not null unique,
+    password varchar(255) not null
+)
